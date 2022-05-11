b3d7724a2442134a0803054e21862677908b5f8a
yabadeer <yabadeer@uwaterloo.ca>
2022-05-10 20:57:41 -0400

    Changed greeting

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -135,7 +135,7 @@ class Home extends Component {
           >
             {this.state.mode === 0 ? (
               <React.Fragment>
-                Welcome to MSCI245!
+                Welcome to MSCI245!!
               </React.Fragment>
             ) : (
               <React.Fragment>
