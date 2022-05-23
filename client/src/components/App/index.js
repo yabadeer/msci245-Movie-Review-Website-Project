dcac4d833fcecdf856b6246239f8a1c40fe935c0
yabadeer <yabadeer@uwaterloo.ca>
2022-05-22 22:57:44 -0400

    Lecture 3 filtering

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,17 +1,91 @@
 import * as React from 'react';
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
-function App() {
+
+const App = () => {
+
+  const textbooks = [
+    {
+      title: 'The Road to React',
+      url: 'https://www.roadtoreact.com/',
+      author: 'Wieruch, R.',
+      year: "2021",
+    }, {
+      title: 'Learning Node.js',
+      url: 'https://github.com/marcwan/LearningNodeJS',
+      author: 'Wandschneider, Marc',
+      year: "2017",
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedTextbooks = textbooks.filter(function (book) {
+    return book.title.includes(searchTerm);
+  });
+
+
+
   return (
     <div>
       <h1>
-        {welcome.greeting} {welcome.title}
+        React Lecture 3_2
       </h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+
+      <Search onSearch={handleSearch} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+
+      <hr />
+
+      <List list={searchedTextbooks} />
+
     </div>
   );
 }
+
+const Search = (props) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input
+      id="search"
+      type="text"
+      onChange={props.onSearch}
+    />
+
+  </div>
+);
+
+
+
+const List = (props) => {
+  return (
+    <ul>
+      {props.list.map((item) => {
+        return (
+          <Item item={item} />
+        );
+      })}
+    </ul>
+
+  )
+}
+
+const Item = (props) => {
+  return (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.url}</a>
+      </span>
+      <span> {props.item.title}</span>
+      <span>{" by " + props.item.author}</span>
+    </li>
+  )
+}
+
+
 export default App;
