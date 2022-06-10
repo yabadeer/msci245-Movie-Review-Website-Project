b2426ca451aca218711634b502913b16a18c7d71
yabadeer <yabadeer@uwaterloo.ca>
2022-06-10 12:14:01 -0400

    Port Number

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -9,10 +9,10 @@ import Paper from "@material-ui/core/Paper";
 
 
 //Dev mode
-const serverURL = ""; //enable for dev mode
+//const serverURL = ""; //enable for dev mode
 
 //Deployment mode instructions
-//const serverURL = "http://ov-research-4.uwaterloo.ca:PORT"; //enable for deployed mode; Change PORT to the port number given to you;
+const serverURL = "http://ov-research-4.uwaterloo.ca:3025"; //enable for deployed mode; Change PORT to the port number given to you;
 //To find your port number: 
 //ssh to ov-research-4.uwaterloo.ca and run the following command: 
 //env | grep "PORT"
