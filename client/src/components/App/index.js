249107a337e15d048e327f7b04436f288868031b
yabadeer <yabadeer@uwaterloo.ca>
2022-06-10 12:00:01 -0400

    Reverting

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,43 +1,65 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
+import * as React from 'react';
 
-import Home from '../Home';
-import PrivateRoute from '../Navigation/PrivateRoute.js';
+const App = () => {
 
-
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      //
-    };
-  }
-
-  componentDidMount() {
-    //
-  }
+  const recipes = [
+    {
+      title: 'Fruit salad',
+      difficulty: '2',
+      ingredients: ['apple', 'banana', 'blueberries', 'raisins', 'walnuts'],
+      calories: "200",
+      instructions: "Wash fresh fruit. Slice fruit into pieces. Mix all ingredients in a bowl."
+    }, {
+      title: 'Avocado wrap',
+      difficulty: '3',
+      ingredients: ['avocado', 'spinach', 'pine nuts', 'mayo', 'apple', 'tortilla bread'],
+      calories: "400",
+      instructions: "Wash all fruits and vegetables. Slice avocadoes and apples. Mix all ingredients and wrap them in a tortilla bread."
+    },
+  ];
 
 
-  componentWillUnmount() {
-    this.listener();
-  }
 
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <PrivateRoute exact path="/" component={Home} />
-        </div>
-      </Router>
-    );
-  }
+  return (
+    <div>
+      <h1>
+        Recipe finder
+      </h1>
+      <ul>
+        {recipes.map((item) => {
+          return (
+            <li>
+              <p> {item.title}</p>
+              
+              <p> {item.difficulty}</p>
+              <p>
+
+                <ul>
+                  {item.ingredients.map((ingredient) => {
+                    return (
+                      <li>{ingredient}</li>
+                    )
+                  }
+                  )}
+                </ul>
+              </p>
+              <p> {item.calories}</p>
+              <p> {item.instructions}</p>
+            </li>
+          )
+
+        })
+        }
+      </ul>
+
+
+
+    </div>
+  );
 }
+
+
+
 
 export default App;
