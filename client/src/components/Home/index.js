dc337fc20d353f74f8a7ae0658cc6ebe86b13a8c
yabadeer <yabadeer@uwaterloo.ca>
2022-07-15 08:36:28 -0400

    Submitting D2

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -23,7 +23,7 @@ import {
 //const serverURL = ""; //enable for dev mode
 
 //Deployment mode instructions
-const serverURL = "http://ec2-18-188-101-79.us-east-2.compute.amazonaws.com:3025"; //enable for deployed mode; Change PORT to the port number given to you;
+const serverURL = "http://ec2-18-216-101-119.us-east-2.compute.amazonaws.com:3025"; //enable for deployed mode; Change PORT to the port number given to you;
 //To find your port number: 
 //ssh to ov-research-4.uwaterloo.ca and run the following command: 
 //env | grep "PORT"
